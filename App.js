51832b2839656a3ae09917a4dddd4da32343a363
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2023-03-21 22:26:02 +0200

    load fonts

--- App.js
+++ App.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import RobotoRegular from "./src/assets/fonts/Roboto-Regular.ttf";
+import RobotoMedium from "./src/assets/fonts/Roboto-Medium.ttf";
+import RobotoBold from "./src/assets/fonts/Roboto-Bold.ttf";
+
 import RegistrationScreen from "./src/Screens/RegistrationScreen";
 
-export default function App() {
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "Roboto-Regular": RobotoRegular,
+    "Roboto-Medium": RobotoMedium,
+    "Roboto-Bold": RobotoBold,
+  });
+};
+
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
   return <RegistrationScreen />;
-}
+};
+
+export default App;
