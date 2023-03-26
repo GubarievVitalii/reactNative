33c131a885fc317ba0e1bf8167348561e550d700
gubariev-vitalii <100084830+GubarievVitalii@users.noreply.github.com>
2023-03-26 14:00:03 +0300

    add MapScreen

--- src/Screens/NestedScreens/MapScreen.js
+++ src/Screens/NestedScreens/MapScreen.js
@@ -1,4 +1,31 @@
+import { useRoute } from "@react-navigation/native";
+import React from "react";
+import { View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
 const MapScreen = () => {
-  return null;
+  const route = useRoute();
+
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: route.params.latitude,
+          longitude: route.params.longitude,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.006,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: route.params.latitude,
+            longitude: route.params.longitude,
+          }}
+          title="travel photo"
+        />
+      </MapView>
+    </View>
+  );
 };
 export default MapScreen;
